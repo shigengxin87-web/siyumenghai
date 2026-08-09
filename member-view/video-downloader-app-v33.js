5c3d1a4c38fbe7812ca2bd5b437a268727658f2c
shigengxin87-web <shigengxin87@gmail.com>
2026-08-09 20:52:19 +0800

    fix: retain cached copies for full history

--- member-view/video-downloader-app-v33.js
+++ member-view/video-downloader-app-v33.js
@@ -1,1 +1,1 @@
-document.write('<script src="./video-downloader-app.js?v=20260809-history-copies"><\/script>');
+document.write('<script src="./video-downloader-app.js?v=20260809-history-copies-2"><\/script>');
